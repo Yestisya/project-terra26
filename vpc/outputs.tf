d0cdb162bd9bf85cf146057897cb96b70f02cb47
Yestisya <yadritama@gmail.com>
2026-03-12 06:43:37 +0000

    Delete vpc directory

--- vpc/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "vpc_id" {
-  value = aws_vpc.project_terra26_vpc.id
-}
-
-output "subnet_id" {
-  value = aws_subnet.project_terra26_subnet.id
-}
--- vpc/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "aws_region" {
-  default = "ap-southeast-1"
-}
-
-variable "az" {
-  default = "ap-southeast-1a"
-}
