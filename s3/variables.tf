44c85af7d680753342c15ff5f436ad026fbfff53
Yestisya <yadritama@gmail.com>
2026-03-12 06:44:07 +0000

    Delete s3 directory

--- s3/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {
-  description = "AWS Region"
-  default     = "ap-southeast-1"
-}
-
-variable "bucket_name" {
-  description = "Name of S3 bucket"
-  type        = string
-}
